69fdd130fdc0cc84ca480d04447f219b2d0e67c3
Bardas Oleksandr <iskander0119@gmail.com>
2018-11-23 00:23:35 +0200

    MessageResolver was refactored

--- src/main/java/ua/levelup/controller/support/MessageResolver.java
+++ src/main/java/ua/levelup/controller/support/MessageResolver.java
@@ -1,11 +1,27 @@
 package ua.levelup.controller.support;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Properties;
 
 @Component("messageResolver")
 public class MessageResolver {
 
-    public String resolveMessageForException(Exception e){
-        return "nothing_have_to_do_with_problem";
+    @Autowired
+    private Properties messagesProperties;
+
+    public String resolveMessageForException(Exception e) {
+        String message = e.getMessage();
+        if (message.equals(messagesProperties.getProperty("NOT_UNIQUE_USER"))) {
+            return "not_unique_user";
+        } else if (message.equals(messagesProperties.getProperty("NOT_UNIQUE_PRODUCT"))) {
+            return "not_unique_product";
+        } else if (message.equals(messagesProperties.getProperty("NOT_UNIQUE_CATEGORY"))) {
+            return "not_unique_category";
+        } else if (message.equals(messagesProperties.getProperty("NOT_UNIQUE_MANUFACTURER"))) {
+            return "not_unique_manufacturer";
+        }
+        return "you_should_watch_logs";
     }
 }
